5d424a1d25b7fd0b790b6a04dae100e7ae0ab68a
m1nl <mateusz@nalewajski.pl>
2026-07-12 15:21:11 +0200

    map extra joy / pad buttons to space, lalt, comma, period, esc

--- fw/ctrl_832/usbhid.c
+++ fw/ctrl_832/usbhid.c
@@ -1,5 +1,7 @@
 #include "usbhid.h"
+#include "amiga_rawkey.h"
 #include "c64keys.h"
+#include "config.h"
 #include "menu.h"
 #include "osd.h"
 #include "usbhid_keycodes.h"
@@ -8,11 +10,14 @@
 #ifdef USBHID_DEBUG
 #define DBG(...) printf(__VA_ARGS__)
 #else
-#define DBG(...) do {} while (0)
+#define DBG(...)                                                                                                      \
+    do {                                                                                                              \
+    } while (0)
 #endif
 
-#define BSWAP32(v) ( (((v) & 0xff000000u) >> 24) | (((v) & 0x00ff0000u) >> 8) \
-                   | (((v) & 0x0000ff00u) << 8)  | (((v) & 0x000000ffu) << 24) )
+#define BSWAP32(v)                                                                                                    \
+    ((((v) & 0xff000000u) >> 24) | (((v) & 0x00ff0000u) >> 8) | (((v) & 0x0000ff00u) << 8) |                          \
+     (((v) & 0x000000ffu) << 24))
 
 #define KEYPAGES 5
 static unsigned int usbhid_prevtable[KEYPAGES];
@@ -20,13 +25,21 @@ static unsigned int usbhid_keytable[KEYPAGES];
 
 static int mouse_buttons0 = 0;
 
-int usbhid_testkey(unsigned char code) {
-    return usbhid_keytable[code >> 5] & (1 << (code & 31));
-}
+/* Gamepad button → Amiga raw key mapping */
+static const struct {
+    unsigned char bit;
+    unsigned char ami;
+} gamepad_keymap[] = {
+    {6, RK_Space}, {7, RK_LAlt}, {13, RK_Comma}, {11, RK_Period}, {9, RK_Esc},
+};
 
-static void usbhid_setkey(unsigned char code) {
-    usbhid_keytable[code >> 5] |= 1 << (code & 31);
-}
+#define GAMEPAD_MAP_COUNT (sizeof(gamepad_keymap) / sizeof(gamepad_keymap[0]))
+
+static unsigned int gamepad_prevbits[USBHID_PORTS];
+
+int usbhid_testkey(unsigned char code) { return usbhid_keytable[code >> 5] & (1 << (code & 31)); }
+
+static void usbhid_setkey(unsigned char code) { usbhid_keytable[code >> 5] |= 1 << (code & 31); }
 
 static void usbhid_send(int type, int code) {
     int t = (type << 14) | (mouse_buttons0 << 8) | (code & 0xff);
@@ -36,8 +49,7 @@ static void usbhid_send(int type, int code) {
 static void usbhid_handlekb(unsigned char *pkt) {
     int i, code;
 
-    DBG("KB: mod=%02x keys=%02x %02x %02x %02x %02x %02x\n",
-        pkt[0], pkt[2], pkt[3], pkt[4], pkt[5], pkt[6], pkt[7]);
+    DBG("KB: mod=%02x keys=%02x %02x %02x %02x %02x %02x\n", pkt[0], pkt[2], pkt[3], pkt[4], pkt[5], pkt[6], pkt[7]);
 
     for (i = 0; i < KEYPAGES; ++i) {
         usbhid_prevtable[i] = usbhid_keytable[i];
@@ -79,12 +91,33 @@ static void usbhid_handlekb(unsigned char *pkt) {
 }
 
 static void usbhid_handlemouse(unsigned char *pkt) {
-    DBG("MOUSE: btn=%02x dx=%d dy=%d\n",
-        pkt[0], (signed char)pkt[1], (signed char)pkt[2]);
+    DBG("MOUSE: btn=%02x dx=%d dy=%d\n", pkt[0], (signed char)pkt[1], (signed char)pkt[2]);
 
     mouse_buttons0 = pkt[0];
-    usbhid_send(0, (signed char)pkt[1]);  /* X */
-    usbhid_send(1, (signed char)pkt[2]);  /* Y */
+    usbhid_send(0, (signed char)pkt[1]); /* X */
+    usbhid_send(1, (signed char)pkt[2]); /* Y */
+}
+
+static void usbhid_handlegamepad(int port, unsigned int status) {
+    unsigned int game_bits = (status >> 16) & 0x3fff;
+    unsigned int changed = gamepad_prevbits[port] ^ game_bits;
+    unsigned int i;
+
+    DBG("GAMEPAD port %d: game_bits=%04x changed=%04x\n", port, game_bits, changed);
+
+    for (i = 0; i < GAMEPAD_MAP_COUNT; ++i) {
+        unsigned int mask = 1u << gamepad_keymap[i].bit;
+        if (changed & mask) {
+            if (game_bits & mask) {
+                DBG("GAMEPAD port %d: bit %d ami=%02x down\n", port, gamepad_keymap[i].bit, gamepad_keymap[i].ami);
+                usbhid_send(2, gamepad_keymap[i].ami);
+            } else {
+                DBG("GAMEPAD port %d: bit %d ami=%02x up\n", port, gamepad_keymap[i].bit, gamepad_keymap[i].ami);
+                usbhid_send(2, gamepad_keymap[i].ami | 0x80);
+            }
+        }
+    }
+    gamepad_prevbits[port] = game_bits;
 }
 
 static void usbhid_handleport(int port) {
@@ -108,6 +141,9 @@ static void usbhid_handleport(int port) {
             usbhid_handlekb(pkt);
         else
             usbhid_handlemouse(pkt);
+
+    } else if (typ == GAMEPAD && (config.autofire & 0x4) == 0) { // do map joystick in CD32 mode
+        usbhid_handlegamepad(port, status);
     } else {
         DBG("HID port %d: ignoring typ=%d\n", port, typ);
     }
@@ -116,9 +152,7 @@ static void usbhid_handleport(int port) {
     HW_USBHID_WRITE(port, REG_USBHID_STATUS, USBHID_STATUS_ACK);
 }
 
-__constructor(102.usbhid) void usbhid_init(void) {
-    puts("USB HID init\n");
-}
+__constructor(102.usbhid) void usbhid_init(void) { puts("USB HID init\n"); }
 
 void usbhid_handle(void) {
     usbhid_handleport(0);
