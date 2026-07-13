83b205f0947c03d41062b4f8a57477bce0bf5389
m1nl <mateusz@nalewajski.pl>
2026-07-13 21:57:11 +0200

    add output register to tag ram for improved timing

--- rtl/lattice/dpram_256x32.v
+++ rtl/lattice/dpram_256x32.v
@@ -7,15 +7,15 @@ module dpram_256x32 (
   input  wire           wren_a,
   input  wire [  8-1:0] address_a,
   input  wire [ 32-1:0] data_a,
-  output [ 32-1:0] q_a,
+  output reg  [ 32-1:0] q_a,
   input  wire           wren_b,
   input  wire [  8-1:0] address_b,
   input  wire [ 32-1:0] data_b,
-  output [ 32-1:0] q_b
+  output reg  [ 32-1:0] q_b
 );
 
 // memory
-reg [32-1:0] mem [0:256-1] /* synthesis syn_ramstyle="block_ram"*/;
+reg [32-1:0] mem [0:256-1] /* synthesis syn_ramstyle="block_ram" */;
 
 reg [32-1:0] q_a_i;
 reg [32-1:0] q_b_i;
@@ -25,14 +25,16 @@ always @ (posedge clock) begin
   if (wren_a) mem[address_a] <= #1 data_a;
   q_a_i <= #1 mem[address_a];
 end
-assign q_a = q_a_i;
+always @ (posedge clock)
+  q_a <= q_a_i;
 
 // port b
 always @ (posedge clock) begin
   if (wren_b) mem[address_b] <= #1 data_b;
   q_b_i <= #1 mem[address_b];
 end
-assign q_b = q_b_i;
+always @ (posedge clock)
+  q_b <= q_b_i;
 
 endmodule
 
